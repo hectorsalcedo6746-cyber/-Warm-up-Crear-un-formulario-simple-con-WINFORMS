30780511c5b0494e25f423ee85f717900a4e4586
hectorsalcedo6746-cyber <hector.salcedo6746@alumnos.udg.mx>
2026-03-10 17:41:25 -0600

    Add files via upload

---
+++ Hola_Mundo/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Hola_Mundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPassword1 = new System.Windows.Forms.TextBox();
+            this.txtPassword2 = new System.Windows.Forms.TextBox();
+            this.bttnValidar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPassword1
+            // 
+            this.txtPassword1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtPassword1.Location = new System.Drawing.Point(271, 26);
+            this.txtPassword1.Name = "txtPassword1";
+            this.txtPassword1.Size = new System.Drawing.Size(235, 22);
+            this.txtPassword1.TabIndex = 0;
+            // 
+            // txtPassword2
+            // 
+            this.txtPassword2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.txtPassword2.Location = new System.Drawing.Point(271, 84);
+            this.txtPassword2.Name = "txtPassword2";
+            this.txtPassword2.Size = new System.Drawing.Size(235, 22);
+            this.txtPassword2.TabIndex = 1;
+            // 
+            // bttnValidar
+            // 
+            this.bttnValidar.Location = new System.Drawing.Point(363, 135);
+            this.bttnValidar.Name = "bttnValidar";
+            this.bttnValidar.Size = new System.Drawing.Size(143, 23);
+            this.bttnValidar.TabIndex = 2;
+            this.bttnValidar.Text = "VALIDAR";
+            this.bttnValidar.UseVisualStyleBackColor = true;
+            this.bttnValidar.Click += new System.EventHandler(this.bttnValidar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Escribe Contraseña:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Repite Contraseña:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(532, 193);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bttnValidar);
+            this.Controls.Add(this.txtPassword2);
+            this.Controls.Add(this.txtPassword1);
+            this.Name = "Form1";
+            this.Text = "Validador de Contraseñas";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPassword1;
+        private System.Windows.Forms.TextBox txtPassword2;
+        private System.Windows.Forms.Button bttnValidar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Hola_Mundo/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hola_Mundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bttnValidar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPassword1.Text) || string.IsNullOrEmpty(txtPassword2.Text))
+            {
+                MessageBox.Show("Ambos campos deben contener texto para validar");
+                return;
+            }
+
+            string contrasena1 = txtPassword1.Text;
+            string contrasena2 = txtPassword2.Text;
+
+            if (!contrasena1.Equals(contrasena2))
+            {
+                MessageBox.Show("Las contraseñas no coinciden, deber ser iguales");
+                return;
+            }
+
+            if (ValidatePasswordComplexity(contrasena1))
+            {
+                MessageBox.Show("CONTRASEÑA VALIDA");
+            }
+            else
+            {
+                MessageBox.Show("LA CONTRASEÑA NO SE CUMPLE CON LAS ESPECIFICACIONES");
+            }
+        }
+        public bool ValidatePasswordComplexity(string password)
+
+        {
+
+            // Regex:
+            // (?[a-z]): Al menos una minúscula
+            // (?.[A-Z]): Al menos una mayúscula
+            // (?=.*\d): Al menos un número
+            // (?.*[^\da-zA-Z]): Al menos un simbolo (no alfanumérico)
+            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).+$";
+            return Regex.IsMatch(password, pattern);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    
+}
